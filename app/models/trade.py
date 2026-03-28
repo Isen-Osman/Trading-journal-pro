3747c57354dd0a15fe4dcdd359e16c81fd3a9e84
isendev <isen.osman252@gmail.com>
2026-03-28 16:47:55 +0100

    Add models

---
+++ app/models/trade.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+##типовите за колони (како SQL типови: int, string, float...)
+from sqlalchemy.orm import relationship
+##Се користи за врска меѓу табели (User ↔ Trade)
+from app.core.database import Base
+## base e osnovata od koja se pravat site modeli
+import datetime
+
+
+class Trade(Base):
+    ## imeto na tabelata
+    __tablename__ = "trades"
+
+    ## index = pobrzo prebaruvanje
+    id = Column(Integer, primary_key=True, index=True)
+    symbol = Column(String, index=True)
+    entry_price = Column(Float)
+    exit_price = Column(Float, nullable=True)
+    quantity = Column(Float)
+    side = Column(String)  # BUY or SELL
+    status = Column(String)  # OPEN or CLOSED
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    user = relationship("User", back_populates="trades")
+    ##pravi vrska pomegu User modelot
